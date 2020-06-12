2f96784a3e4c6330097dfbc0ab71e900ed0a7f77
jbruel <jonathan.bruel@epitech.eu>
2020-06-12 18:32:04 +0200

    Setup project with cmakelist (win / unix) + bin/src directories

---
+++ src/askia/main.cpp
@@ -0,0 +1,14 @@
+//!
+//! @author jbruel
+//! @date 12/06/2020
+//! @brief Main function for the Askia technical test.
+//!
+
+#include <cstdlib>
+#include <iostreamgit>
+
+int main(int argc, const char * const *argv)
+{
+    std::cout << "Hello World!" << std::endl;
+    return (EXIT_SUCCESS);
+}
